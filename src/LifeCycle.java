913d5c128cd9222e12ca908232957c5e6e1f459c
soumen <soumenmondal7584@gmail.com>
2026-02-27 09:59:43 +0530

    i have learn Interrupt() yield()

--- src/LifeCycle.java
+++ src/LifeCycle.java
@@ -1,10 +1,33 @@
 public class LifeCycle extends Thread{
+    public  LifeCycle(String name){//Provide the thread Name
+        super(name);
+    }
+    public LifeCycle(){
+
+    }
+//    @Override
+//    public void run() {
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     @Override
-    public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+//    public void run() { // Interrupt()
+//        try {
+//            System.out.println("Thread is sleeping...");
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            System.out.println("Thread was interrupted!");
+//        }
+//    }
+    public void run() {//    yield()
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+            Thread.yield();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,48 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
     System.out.println("Multithreading in Java");
 
-     /*   Thread  Life Cycle */
-        LifeCycle lifeCycle=new LifeCycle(); //NEW State
-        System.out.println(lifeCycle.getState());
-        lifeCycle.start(); // RUNNABLE OR RUNING
-        System.out.println(lifeCycle.getState());
+  /*   Thread  Life Cycle */
 
-        Thread.sleep(100);
-        System.out.println(lifeCycle.getState());//TIMED_WAITING
+//        LifeCycle lifeCycle=new LifeCycle("soumen"); //NEW State
+//        System.out.println(lifeCycle.getState());
+//        lifeCycle.start(); // RUNNABLE OR RUNNING
+//        System.out.println(lifeCycle.getState());
+//
+//        Thread.sleep(100);
+//        System.out.println(lifeCycle.getState());//TIMED_WAITING
+//
+//        lifeCycle.join(); // waiting the main to completed lifeCycle
+//        System.out.println(lifeCycle.getState()); //TERMINATED
 
-        lifeCycle.join();
-        System.out.println(lifeCycle.getState()); //TERMINATED
+
+ /* Thread Priority */
+
+//        LifeCycle t1 =new LifeCycle("Thread 1");
+//        LifeCycle t2 =new LifeCycle("Thread 2");
+//        LifeCycle t3 =new LifeCycle("Thread 3");
+//        t1.setPriority(Thread.NORM_PRIORITY);
+//        t2.setPriority(Thread.MIN_PRIORITY);
+//        t3.setPriority(Thread.MIN_PRIORITY);
+
+/* Interrupt()   yield()  */
+
+        /* interrupt() is used to stop or signal a thread that it should stop working.It does NOT forcefully stop the thread.
+        It just sends a signal. */
+
+//        LifeCycle t = new LifeCycle("Interrupt");
+//        t.start();
+//        t.interrupt();
+
+//        yield() tells the scheduler: "I am ready to pause. You can give chance to another thread."
+
+        LifeCycle t1 = new LifeCycle();
+        LifeCycle t2 = new LifeCycle();
+
+        t1.start();
+        t2.start();
+
+
+
+
     }
 }
